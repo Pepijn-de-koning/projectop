f96f65fcdbc0df1199d59eef068c78cf778af47e
Pepijn-de-koning <54800145+Pepijn-de-koning@users.noreply.github.com>
2020-01-12 22:54:32 +0100

    Add files via upload

---
+++ code/index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <title>Mediadeveloper</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/text.css">
+        <link rel="stylesheet" href="css/slideshow.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    </head>
+<section>
+    <heading>
+        <video src="img/coding.mp4" autoplay muted loop id="video"></video>
+        <img class="logo" src="img/ma.jpg" width="200px">
+    </heading>
+
+  <body>
+        <div class="mediacollege">
+            <p><span>M</span>ediacollege<br> Amsterdam</p>
+        </div>
+
+      <div class="typewriter">
+          <div class="typewriter-text"><span>M</span>edia developer worden?</div>
+      </div>
+
+</section>
+      <div class="text1">
+            <p class="p"><a class="headersize">De opleiding</a> <br><br>
+              Als mediadeveloper programmeer en onderhoud je
+              (mobiele) websites, apps en webapplicaties. Je
+              leert niet alleen veel over technologie en
+              programmeren, maar ook over de basis van digitale
+              vormgeving. Want een product moet niet alleen
+              goed werken, ook het idee erachter moet goed
+              zijn en het product moet er aantrekkelijk uitzien.
+              Ook leer je samenwerken met mediavormgevers,
+              want zij nemen later de vormgeving voor hun rekening.
+              Je opleiding begint met een periode waarin je samen met
+              studenten van de opleiding Gamedeveloper de lessen
+              volgt. Na deze periode kies je ervoor om verder te
+              studeren als mediadeveloper.
+          </p>
+      </div>
+      <iframe src="https://player.vimeo.com/video/145398347" width="1040" height="760" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+      <p><a href="https://vimeo.com/145398347"></a><a href="https://vimeo.com/user15208661"></a><a href="https://vimeo.com"></a></p>
+
+
+      <img class="ma" src="img/mediacollege.jpg" width="1000px" height="750px;">
+      <div class="text2">
+            <p class="p"><a class="headersize">De school</a> <br><br>
+              Ma helpt je jouw speciale talent te ontdekken
+              en daar je beroep van te maken. We kennen de
+              bedrijven in en rond Amsterdam en weten wat
+              jouw toekomstige werkgevers belangrijk vinden.
+              Want Ma is een vakschool. Al ruim 100 jaar
+              leiden we studenten zo op dat ze direct aan
+              het werk kunnen. Tijdens je opleiding koppelen
+              we je aan bedrijven. En we laten je samenwerken
+              aan projecten met studenten van andere
+              opleidingen. Bij dit alles begeleiden we je
+              intensief. Want we willen dat alle studenten
+              hun diploma halen
+          </p>
+      </div>
+
+      <div class="text3">
+            <p class="p"><a class="headersize">Amsterdam</a> <br><br>
+              Kies je voor Ma, dan kies je voor Amsterdam.
+              Dat betekent dat je je opleiding volgt in de
+              best denkbare omgeving. Ga maar na: in Amsterdam
+              en omgeving zit een aantal van de beste internet-
+              en mediabedrijven ter wereld. En, ze kunnen
+              hardwerkende jonge professionals die zich
+              gespecialiseerd hebben in web- en appdevelopment
+              goed gebruiken. Een stage en wellicht later een
+              baan vinden is hier momenteel geen probleem.
+              Bovendien heeft Amsterdam alles om een goede studietijd
+              te beleven!
+            </p>
+      </div>
+      <img class="amsterdam" src="img/amsterdam.jpg" width="1000px" height="750px;">
+
+      <img class="work" src="img/work.jpg" width="1000px" height="750px;">
+      <div class="text4">
+            <p class="p"><a class="headersize">Wat na MA</a> <br><br>
+              Werken
+              Bedrijven zitten momenteel te springen om goede programmeurs.
+              Na je opleiding kun je als web- of appdeveloper gaan werken bij
+              bedrijven die websites en apps ontwikkelen. <br> <br>
+
+              <a class="headersize">Door studeren</a> <br> <br>
+              Met een diploma niveau 4 kun je doorstuderen op een aantal
+              hbo's, zoals Software Engineering bij de Hogeschool van
+              Amsterdam. Je zult merken dat je met de bij Ma opgedane
+              vakkennis een vliegende start maakt in het hbo.
+          </p>
+        </div>
+
+        <div class="text5">
+          <p class="p"><a class="headersize">Opendagen</a> <br><br>
+              Ma helpt als jij het wilt maken in de media. Met opleidingen
+              die je voorbereiden op jouw toekomstige beroep. Om hier alvast
+              kennis mee te maken bezoek je natuurlijk één van onze open
+              dagen. Dat is dé manier om de school en de beroepen waar we
+              voor opleiden te leren kennen. Kijk of jij er tussen past.
+              Kom langs, proef de sfeer, ontmoet medewerkers en studenten,
+              en bekijk hun werk in onze lokalen en studio’s.
+              <br> <br>
+              mbo (Contactweg 36): <br>
+              woensdag 22 januari 2020 van 15.00 tot 20.30 uur <br> <br>
+              vmbo (Dintelzaal 15): <br>
+              woensdag 22 januari 2020 van 15.00 tot 20.30 uur <br>
+              woensdag 12 februari 2020 van 15.00-20.30 uur
+
+          </p>
+        </div>
+
+
+    <section>
+
+      <div class="text6">
+        <p class="p"><a class="headersize">Toelatings eisen</a><br><br><br>
+          Niveau:     Minimaal vmbo-kb <br> <br>
+          Leerjaren:  4 Jaar <br> <br>
+          Leerweg:    Bol <br> <br>
+          Niveau:     4 <br> <br>
+          Kwalificatiedossier:  Software development <br> <br>
+          Maximum aantal: 60 plaatsen in totaal
+        </p>
+      </div>
+
+      <section></section>
+
+      <section id="Contact">
+
+        <p class="inschrijven"><a class="headersize">Inschrijven</a> <br><br>
+          Is Mediadeveloper (Software developer) de opleiding voor jou? <br> <br>
+          Meld je nu aan voor studiejaar 2020-2021! <br> <br>
+
+          <a class="a" href="https://www.ma-web.nl/aanmelden/aanmelden-mbo/?opleiding=mediadeveloper#aanmelden">Meld je hier aan</a>
+        </p>
+
+        <div class="container">
+            <form action="contactform.php" method="post" class="contact">
+              <p class="formname">Neem contact met ons op</p>
+               <label class="label" for="naam">Naam</label>
+               <input type="text" class="geel" name="naam">
+
+               <label class="label" for="email">E-Mail</label>
+               <input type="text" class="geel" name="email">
+
+               <label class="label" for="onderwerp">Bericht</label>
+               <textarea class="geel" name="onderwerp" style="height:200px"></textarea>
+
+               <div class="submitbutten">
+                  <input class="submit" name="submit" type="submit" value="Submit">
+               </div>
+            </form>
+
+        </div>
+
+      </section>
+
+    <footer>
+        <div>
+          <p>
+            <span>M</span>ediacollege Amsterdam
+          </p>
+        </div>
+        <div>
+          Contactweg 36 1014 AN Amsterdam <br>
+          020-850-95-00 <br>
+          info.ma-web.nl <br>
+        </div>
+        <div>
+          <p>
+            Volg ons: <a class="social" href="https://www.facebook.com/MediacollegeAmsterdam" target="_blank">Facebook</a> / <a class="social" href="https://www.instagram.com/mediacollege/?hl=nl" target="_blank">Instagram</a> / <a class="social" href="https://twitter.com/mediacollege" target="_blank">Twitter</a> / <a class="social" href="https://www.linkedin.com/school/mediacollege-amsterdam/" target="_blank">Linked in</a>
+          </p>
+        </div>
+    </footer>
+  </body>
+</html>
